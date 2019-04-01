0978fd00b3bd1e53bf0e03c7cbf82f6223f3672f
Kent Ketter <kent.ketter1@gmail.com>
2019-04-01 14:37:14 -0700

    scaffold

---
+++ app.js
@@ -0,0 +1,5 @@
+//Global Values
+var hours = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm',
+'5pm','6pm','7pm','8pm'];
+var locations = ['1st and Pike','SeaTac Airport','Seattle Center','Capitol Hill','Alki'];
+

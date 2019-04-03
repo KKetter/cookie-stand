3d13a499a38225ebc0231dc7a34d80116d6965af
Kent Ketter <kent.ketter1@gmail.com>
2019-04-03 15:20:58 -0700

    need to divert planning

--- app.js
+++ app.js
@@ -5,47 +5,57 @@ var locations = [];
 var salesTable = document.getElementById('sales');
 // Hard code this to populate as the table head with a for loop
 var hours = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm'];
-// function makeHeaderRow(){
-//     var trEl = document.createElement('tr');
-//     var thEl = document.createElement('th');
-//     thEl.textContent = hours[0];
 
-// }
-function generate_table() {
-    // get the reference for the body
-    var body = document.getElementById("body");
+// //make table body
+// function generate_table() {
+//     // get the reference for the body
+//     //var body = document.getElementById("body");
   
-    // creates a <table> element and a <tbody> element
-    var tbl = document.createElement("table");
-    var tblBody = document.createElement("tbody");
-  
-    // creating all cells
-    for (var i = 0; i < locations.length; i++) {
-      // creates a table row
-      var row = document.createElement("tr");
-  
-      for (var j = 0; j < hours.length; j++) {
-        // Create a <td> element and a text node, make the text
-        // node the contents of the <td>, and put the <td> at
-        // the end of the table row
-        var cell = document.createElement("td");
-        //below line should push an hourlycookiesarray - one at a time
-        var cellText = document.createTextNode(storeOne.hourlyCookies[i]);
-        //cell.textContent = "cell in row ";
-        cell.appendChild(cellText);
-        row.appendChild(cell);
-      }
-  
-      // add the row to the end of the table body
-      tblBody.appendChild(row);
-    }
-  
-    // put the <tbody> in the <table>
-    tbl.appendChild(tblBody);
-    // appends <table> into <body>
-    body.appendChild(tbl);
-    // sets the border attribute of tbl to 2;
-  };
+//     // creates a <table> element and a <tbody> element
+//     var tbl = document.createElement("table");
+//     //var tblBody = document.createElement("tbody");
+    
+//     //tblHeadData.textContent = this.locationName;
+//     for (let index = 0; index < locations.length; index++) {
+//         var trEl = document.createElement("tr");
+//         var tblHeadData = document.createElement("tr");
+//         tblHeadData.textContent = locations[index].locationName;
+//         console.log('info', locations[index].locationName); 
+//         trEl.appendChild(tblHeadData);
+//         //where do we append this? Tried:tblBody(all in 1x1 position)
+//         tbl.appendChild(trEl); 
+//         //var tr = document.createElement("tr");
+//     }
+//     createHeaderRow(tbl);
+//     // creating all body cells
+//     for (var i = 0; i < locations.length; i++) {
+//       // creates a table row
+//       var row = document.createElement("tr");
+//       for (var j = 0; j < hours.length; j++) {
+//         var cell = document.createElement("td");
+//         var cellText = document.createTextNode(locations[i].hourlyCookies[j]);
+//         cell.appendChild(cellText);
+//         row.appendChild(cell);
+//       }
+//       // add the row to the end of the table body
+//       tbl.appendChild(row);
+//     }
+//     // put the <tbody> in the <table>
+//     //tbl.appendChild(tblBody);
+//     // appends <table> into <body>
+//     body.appendChild(tbl);
+//   };
+
+//   //create table header
+//   function createHeaderRow(tbl){
+//     var tr = document.createElement("tr");
+//     tbl.appendChild(tr);
+//     for (let index = 0; index < hours.length; index++) {
+//         var header = document.createElement("th");
+//         header.textContent = hours[index];
+//         tr.appendChild(header);
+//     }
+//   };
 
 function storeConstructor(locationName,minCustPerHour,maxCustPerHour,avgCookiesPerCust){
     this.locationName = locationName;
@@ -86,21 +96,15 @@ function storeConstructor(locationName,minCustPerHour,maxCustPerHour,avgCookiesP
 };
 var storeOne = new storeConstructor('1st and Pike',23,65,6.3);
 storeOne.calculateHourlyCookieSales();
-//storeOne.render();
 var storeTwo = new storeConstructor('SeaTac Airport',3,24,1.2);
 storeTwo.calculateHourlyCookieSales();
-//storeTwo.render();
 var storeThree = new storeConstructor('Seattle Center',11,38,3,7);
 storeThree.calculateHourlyCookieSales();
-//storeThree.render();
 var storeFour = new storeConstructor('Capitol Hill',20,38,2.3);
 storeFour.calculateHourlyCookieSales();
-//storeFour.render();
 var storeFive = new storeConstructor('Alki',2,16,4.6);
 storeFive.calculateHourlyCookieSales();
-//storeFive.render();
-//makes table - please order after constructors
-generate_table();
+
 
 
 
